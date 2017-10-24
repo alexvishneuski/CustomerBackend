fe035b4ad6346d6902aadbb242d7ae77aa8a69c2
alexvishneuski <alexvishneuski@gmail.com>
2017-10-25 02:54:29 +0300

    Added CheckAppVersionActivity + layout. Made conversion from mainAktivity to CheckAppconversion

--- app/src/main/java/com/github/alexvishneuski/customerbackend/MainActivity.java
+++ app/src/main/java/com/github/alexvishneuski/customerbackend/MainActivity.java
@@ -10,10 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startCustomerActivity();
+        startCheckVersionActivity();
+
+//        startCustomerActivity();
+
     }
+
+    private void startCheckVersionActivity() {
+        startActivity(new Intent(this, CheckVersionActivity.class));
+    }
+
 
     private void startCustomerActivity() {
         startActivity(new Intent(this, CustomerActivity.class));
     }
+
+
 }
